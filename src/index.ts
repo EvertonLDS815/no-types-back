832e16005c5edbbac1eb7cbcb701702489e5b1cd
EvertonLDS815 <billseverton12345@gmail.com>
2023-07-03 08:47:47 -0300

    adicionando o cors

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,14 @@ import path from 'node:path';
 import express from 'express';
 import mongoose from 'mongoose';
 import { router } from './router';
+import cors from 'cors';
 
 mongoose.connect(`mongodb+srv://evertonnorope:norope@cluster0.fes6jnl.mongodb.net/?retryWrites=true&w=majority`)
 	.then(() => {
 		const app = express();
 		const port = process.env.PORT || 3001;
 
+		app.use(cors());
 		app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')));
 		app.use(express.json());
 		app.use(router);

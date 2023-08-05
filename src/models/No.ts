0f5030ce4745860df5c27b5ed70ef80ddc9e7b18
EvertonLDS815 <evertonlino1234@gmail.com>
2023-08-05 20:37:13 -0300

    feat: complete CRUD

--- src/models/No.ts
+++ src/models/No.ts
@@ -1,27 +1,30 @@
-import {Schema, model} from 'mongoose';
+import { Schema, model } from 'mongoose';
 
-export const No = model('No', new Schema({
-	name: {
-        type: String,
-        require: true
-    },
-    nivel: {
-        type: String,
-		enum: ['Básico', 'Intermediário', 'Avançado'],
-        default: 'Básico'
-    },
-    type: {
-        type: String,
-    },
-    linkImage: {
-        type: String,
-        require: true
-    },
-    linkVideo: {
-        type: String,
-    },
-    description: {
-        type: String,
-        require: true
-    },
-}))
+export const No = model(
+	'No',
+	new Schema({
+		name: {
+			type: String,
+			require: true,
+		},
+		nivel: {
+			type: String,
+			enum: ['Básico', 'Intermediário', 'Avançado'],
+			default: 'Básico',
+		},
+		type: {
+			type: String,
+		},
+		linkImage: {
+			type: String,
+			require: true,
+		},
+		linkVideo: {
+			type: String,
+		},
+		description: {
+			type: String,
+			require: true,
+		},
+	}),
+);
